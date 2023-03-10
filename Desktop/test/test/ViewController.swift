2bfe2d56f8fea5b9a5f734be03fa56d3910ff927
saniya <janbolatovnas@gmail.com>
2023-03-10 21:42:28 +0600

    new message

---
+++ Desktop/test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Dauletkhanova Saniya on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //hahaha
+    }
+
+
+}
+
